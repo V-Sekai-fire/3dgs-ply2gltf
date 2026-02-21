06ac68b0373f4b57ba3c8487ac2dbc332d601a79
Norbert Nopper <norbert.nopper@huawei.com>
2026-02-20 17:17:09 -0800

    Fixes and suggestion related to pull request
    see https://github.com/NorbertNopper-Huawei/3dgs_ply2gltf/pull/1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <cmath>
 #include <cstdint>
+#include <cstdio>
 #include <limits>
 #include <sstream>
 #include <string>
@@ -433,11 +434,15 @@ int main(int argc, char* argv[])
         }
         else if (line.find("element vertex") != std::string::npos)
         {
-            auto result = sscanf_s(line.c_str(), "element vertex %u", &count);
+            auto result = std::sscanf(line.c_str(), "element vertex %u", &count);
             if (result < 0)
             {
                 return -1;
             }
+        }
+        else if (line.find("comment") != std::string::npos)
+        {
+            continue;
         }
         else if (line == "end_header")
         {
@@ -448,7 +453,7 @@ int main(int argc, char* argv[])
             char componentType[256u];
             char name[256u];
 
-            auto result = sscanf_s(line.c_str(), "property %s %s", componentType, 256u, name, 256u);
+            auto result = std::sscanf(line.c_str(), "property %255s %255s", componentType, name);
             if (result < 0)
             {
                 return -1;
@@ -638,7 +643,7 @@ int main(int argc, char* argv[])
             float z{sourceData[3u]}; 
 
             // Also normalize it, as not given by PLY.
-            float norm = std::sqrtf(x*x + y*y + z*z + w*w);
+            float norm = std::sqrt(x*x + y*y + z*z + w*w);
             if (norm == 0.0f)
             {
                 printf("Error: Invalid quaternion\n");
@@ -697,7 +702,7 @@ int main(int argc, char* argv[])
 
             // Sigmoid function needs to be applied before storing.
             const float opacity = *sourceData;
-            *data = 1.0f / (1.0f + std::expf(-opacity));
+            *data = 1.0f / (1.0f + std::exp(-opacity));
 
             byteOffset += 1u * sizeof(float);
         }
